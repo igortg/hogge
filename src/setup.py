6791f257c10b3678986c95fb5fefc051ea78c62d
itghisi <igor.ghisi@gmail.com>
2015-01-24 16:50:30 -0200

    Add py2exe setup script

---
+++ src/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    name='hogge',
+    version='0.1',
+    package_dir={'': 'src'},
+    url='https://github.com/itghisi/hogge',
+    license='LGPL v3.0',
+    author='Igor Ghisi',
+    author_email='igor.ghisi@gmail.com',
+    description='',
+    console=[{
+        "dest_base": "hogge",
+        "script": "hogge/main.py",
+    }],
+    zipfile = None,
+    options={
+        "py2exe": {"optimize": 2,
+                   "compressed": True,
+                   "excludes": "Tkinter",
+                   "bundle_files": 0,
+        }
+    },
+)
